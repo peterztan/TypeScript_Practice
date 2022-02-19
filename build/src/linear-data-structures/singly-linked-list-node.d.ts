6884046b19b7284933889be6dafea54b0a9509a5
peterztan <pt072093@gmail.com>
2022-02-18 22:39:36 -0600

    Revert "add sll-node, sll and respective tests, NOTE: sll failling"
    This reverts commit 564f85614220b020ab14fd211926a4f18294c316.

--- build/src/linear-data-structures/singly-linked-list-node.d.ts
+++
@@ -1,8 +0,0 @@
-export default class SllNode {
-    private data;
-    private next;
-    constructor(data: unknown);
-    setNextNode(node: SllNode): void;
-    getNextNode(): SllNode | null;
-    getData(): unknown;
-}
